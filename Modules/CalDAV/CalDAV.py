186214a365cf0c29a20424516e9d8ed928bf1f5d
Chlo-ee <chloe@chlo.ee>
2025-08-22 12:02:49 +0200

    First draft of CalDAV integration

---
+++ Modules/CalDAV/CalDAV.py
@@ -0,0 +1,31 @@
+import datetime
+
+from caldav.davclient import get_davclient
+from datetime import date, timedelta
+
+config = {}
+
+def get_todays_events(url, username, password):
+    with get_davclient(username=username, password=password,url=url) as client:
+        calendar = client.calendar(url=url)
+
+        events = calendar.search(
+            event=True,
+            start=date.today(),
+            end=date.today() + timedelta(days=1) - timedelta(microseconds=1),
+            expand=True)
+    return events
+
+async def get_data():
+    target = datetime.time.fromisoformat(config['runat'])
+    if datetime.datetime.now().time().hour != target.hour or datetime.datetime.now().time().minute != target.minute:
+        return ''
+    summary = ''
+    for calendar in config['calendars']:
+        events = get_todays_events(calendar['url'], calendar['username'], calendar['password'])
+        if len(events) > 0:
+            summary += f'====================\n{calendar['topic']}:\n'
+            for event in events:
+                summary += f'    Summary: {event.component['summary']}\n    Begin: {event.component['dtstart'].dt.isoformat()}\n    End: {event.component['dtend'].dt.isoformat()}\n\n'
+    print(summary)
+    return summary
---
+++ Modules/CalDAV/__init__.py
@@ -0,0 +1,6 @@
+from Modules.CalDAV.CalDAV import get_data, config
+
+MODULE_NAME = 'CalDAV'
+
+if __name__ == '__main__':
+    print('This module is part of LillyAI and can not be run individually.')
